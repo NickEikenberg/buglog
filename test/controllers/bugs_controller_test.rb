268a5264bedbed73c6592ec91d29e74519bfc8cb
Nick Eikenberg <nickeikenberg95@gmail.com>
2022-01-19 13:24:43 -0500

    All bug and project controller tests pass

--- test/controllers/bugs_controller_test.rb
+++ test/controllers/bugs_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class BugsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @bug = bugs(:testbug)
+    @project = projects(:projectone)
   end
 
   test "should get index" do
@@ -17,7 +18,7 @@ class BugsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create bug" do
     assert_difference('Bug.count') do
-      post bugs_url, params: { bug: { assigned_to: @bug.assigned_to, branch: @bug.branch, description: @bug.description, priority: @bug.priority, reported_by: @bug.reported_by, repro_steps: @bug.repro_steps, severity: @bug.severity, status: @bug.status, title: @bug.title } }
+      post bugs_url, params: { bug: { assigned_to: @bug.assigned_to, branch: @bug.branch, description: @bug.description, priority: @bug.priority, reported_by: @bug.reported_by, repro_steps: @bug.repro_steps, severity: @bug.severity, status: @bug.status, title: @bug.title, project_id: @project.id } }
     end
 
     assert_redirected_to bug_url(Bug.last)
@@ -34,7 +35,7 @@ class BugsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update bug" do
-    patch bug_url(@bug), params: { bug: { assigned_to: @bug.assigned_to, branch: @bug.branch, description: @bug.description, priority: @bug.priority, reported_by: @bug.reported_by, repro_steps: @bug.repro_steps, severity: @bug.severity, status: @bug.status, title: @bug.title } }
+    patch bug_url(@bug), params: { bug: { assigned_to: @bug.assigned_to, branch: @bug.branch, description: @bug.description, priority: @bug.priority, reported_by: @bug.reported_by, repro_steps: @bug.repro_steps, severity: @bug.severity, status: @bug.status, title: @bug.title, project_id: @project.id } }
     assert_redirected_to bug_url(@bug)
   end
 
