4c9c165fd690a5046c01e65399701e59cee5836a
Nick Eikenberg <nickeikenberg95@gmail.com>
2022-01-20 17:34:39 -0500

    users project list now on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       @user = User.find_by_id(current_user.id)
+      @projects = Project.where(user_id: @user.id)
     else
       nil
     end
