8c39c6eb7f972bffbe1f90f4a2a5fc894cc5bd31
Nick Eikenberg <nickeikenberg95@gmail.com>
2022-01-20 17:43:01 -0500

    user projects and bugs appear on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
+    @projects = Project.where(user_id: @user.id)
+    @bugs = Bug.where(user_id: @user.id)
   end
 end
