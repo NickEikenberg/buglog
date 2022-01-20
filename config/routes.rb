bb71fcca180761fd649a91d931948cb989288837
Nick Eikenberg <nickeikenberg95@gmail.com>
2022-01-19 20:31:09 -0500

    Users can access their profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  # get "users/show"
+  # get "users/show/:id", to: "users#show"
+  # get "users/index"
+  resources :users, only: [:index, :show]
   resources :projects
   resources :bugs
   root to: "bugs#index"
