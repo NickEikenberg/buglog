c2be9f5423a19fc95ff7824d60d94b1924dd4dc7
Nick Eikenberg <nickeikenberg95@gmail.com>
2022-01-20 13:03:22 -0500

    User can sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_out_path(resource_or_scope)
+    if current_user
+      request.referrer
+    else
+      root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
+  root "home#index"
   devise_for :users
-  # get "users/show"
-  # get "users/show/:id", to: "users#show"
-  # get "users/index"
   resources :users, only: [:index, :show]
   resources :projects
   resources :bugs
-  root to: "bugs#index"
+
+  devise_scope :user do
+    delete 'logout', to: 'home#index'
+  end
 end
