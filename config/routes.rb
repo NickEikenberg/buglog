3c533e0f4826f787277405c3a80460c5d40564de
Nick Eikenberg <nickeikenberg95@gmail.com>
2022-01-20 14:36:07 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-
   root "home#index"
   devise_for :users
   resources :users, only: [:index, :show]
   resources :projects
   resources :bugs
-
-  devise_scope :user do
-    delete 'logout', to: 'home#index'
-  end
 end
